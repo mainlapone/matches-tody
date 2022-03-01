9774fa20da49c5f8eac621fbb5135a28846e9688
mainlapone <100685100+mainlapone@users.noreply.github.com>
2022-03-01 12:31:53 +0200

    Add files via upload

---
+++ app/hello.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+
+from flask import Flask, render_template
+from flask_cors import CORS,cross_origin
+import requests
+
+def getData():
+    url = requests.get("https://www.as-goal.com/mm/").text
+    x = url.find("<div id=\"Today\"")
+    y = url.find("<div id=\"Tomorrow\"")
+    return  url[x:y]
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/" , methods=['POST', 'GET','OPTIONS'])
+@cross_origin()
+def index():
+    return getData() , 200, {'Content-Type': 'text/html; charset=UTF-8'}
+#     return render_template("index.html", data=getData())
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
+
